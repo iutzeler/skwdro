f697c56a4999679de42cbd39c91b6c430c9ffc10
Vincent Florian <florianvincent@free.fr>
2023-05-16 13:40:03 +0200

    [FEAT][WIP] Optimality conditions interface
    Interface for description of optimality conditions based on :
    * max number of iterations
    * relative change in the parameters (in terms of step p-norm)
    * a combination of both of the above.
    The second and third criteria rely possibly on both the theta and lambda
    changes, with a separate tolerance factor for each, or on only one of
    them if needed.
    
    Comments and documentation to come soon.

---
+++ skwdro/solvers/optim_cond.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+class OptCond:
+    def __init__(self, order, tol_theta: float=1e-4, tol_lambda: float=1e-4, max_iter: int=int(1e4), mode: str="both"):
+        assert order > 0
+        self.p = order
+        self.tol_t = tol_theta
+        self.tol_l = tol_lambda
+        self.maxiter = max_iter
+
+        self.mode = True
+        if mode in {"both", "theta_and_lambda", "t&l"}:
+            self.mode = True
+        elif mode in {"one", "theta_or_lambda", "tUl"}:
+            self.mode = False
+        else:
+            raise ValueError("Please provide a valid string value for parameter 'mode' in OptCond: '" + str(mode) + "' is invalid.")
+
+    def __call__(self, grads, it: int) -> bool:
+        grad_theta, grad_lambda = grads
+        grad_t_stop = float(np.linalg.norm(grad_theta, ord=self.p)) < self.tol_t and self.tol_t  > 0
+        grad_l_stop = float(grad_lambda) < self.tol_l and self.tol_l  > 0
+        it_stop = it >= self.maxiter and self.maxiter > 0
+        return self.grad_stop(grad_t_stop, grad_l_stop) or it_stop
+
+    def grad_stop(self, grad_t_stop: bool, grad_l_stop: bool) -> bool:
+        if self.mode:
+            return grad_t_stop and grad_l_stop
+        else:
+            return grad_t_stop or grad_l_stop
