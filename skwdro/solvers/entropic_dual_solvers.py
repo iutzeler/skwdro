72e2d59d7d4471384114a1bad477e05b19f50d90
Vincent Florian <florianvincent@free.fr>
2023-05-16 13:35:04 +0200

    [FEAT] Full parallelization of code.
    Use of numpy native vectorization to speed up computations by an order
    ~100.
    Every for loop has been removed except for descent algorithms, with
    separate gradient computations.
    
    See next commit(s) for explainations about optimality conditions.

--- skwdro/solvers/entropic_dual_solvers.py
+++ skwdro/solvers/entropic_dual_solvers.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy.random import sample
 
 from skwdro.solvers.utils import *
+from skwdro.solvers.optim_cond import OptCond
 
 # import progressbar
 # widgets = [' [',
@@ -13,9 +14,212 @@ from skwdro.solvers.utils import *
 
 
 
-def WDROEntropicSolver(WDROProblem=None, epsilon=1e-2, Nsamples = 20,fit_intercept=False):
-    return WangGaoXie_v1(WDROProblem=WDROProblem, epsilon=epsilon, Nsamples = Nsamples,fit_intercept=fit_intercept)
+def WDROEntropicSolver(WDROProblem=None, epsilon=1e-2, Nsamples = 20,fit_intercept=False, opt_cond=OptCond(2)):
+    #return WangGaoXie_v1(WDROProblem=WDROProblem, epsilon=epsilon, Nsamples = Nsamples,fit_intercept=fit_intercept)
+    return WangGaoXie_v2(WDROProblem=WDROProblem, epsilon=epsilon, n_samples = Nsamples,fit_intercept=fit_intercept, opt_cond=opt_cond)
 
+def lr_decay_schedule(iter_idx, offset: int=10, lr0=1e-1):
+    return lr0 * (iter_idx + offset)**-0.8
+
+# ### Initializations and sampling #############################
+def init_theta(d):
+    # Glorot init
+    # Fan_in = n or n+1 depending on intercept
+    theta = np.random.randn(d) / np.sqrt(.5 * (d+1))
+    return theta
+
+def prepare_data(samples, m, d, n_samples, epsilon, fit_intercept):
+    # pi_0_samples = sample_pi_0(epsilon, n_samples, samples)
+    if fit_intercept:
+        xi = np.concatenate((np.ones((m, 1)), samples), axis=1)
+        # zeta = np.concatenate((np.ones((n_samples, m, 1)), pi_0_samples), axis=2)
+        theta = init_theta(d + 1)
+    else:
+        xi = samples
+        theta = init_theta(d)
+        # zeta = pi_0_samples
+    zeta = sample_pi_0(epsilon, n_samples, xi)
+    return xi, theta, zeta
+
+def sample_pi_0(epsilon, n_samples, xi):
+    m, d = xi.shape
+
+    sigma = epsilon
+    pi_0_noise = np.random.randn(n_samples, m, d)
+    pi_0_raw_samples = pi_0_noise * sigma + xi[None, :, :]
+
+    # TODO: constrain to Xi_bounds?
+    zeta = pi_0_raw_samples
+    return zeta
+# ##############################################################
+
+# TODO: factorize exp mean code
+# ### Steps ####################################################
+def step_lam_wol(xi, zeta, theta, lam, cost, loss, t, rho, epsilon):
+    c = cost(xi[None, :, :], zeta) # (n_samples, m, 1)
+
+    loss_outputs = loss(theta, zeta)
+    exps_coefs = loss_outputs - lam * c
+    exps_coefs /= epsilon
+
+    minus_full_grads = non_overflow_exp_mean(exps_coefs, c)
+    grad_estimate = rho - minus_full_grads.mean()
+    lr = lr_decay_schedule(t)
+    return -lr * grad_estimate
+
+def step_lam_wl(xi, xi_labels, zeta, zeta_labels, theta, lam, cost, loss, t, rho, epsilon):
+    c = cost(xi[None, :, :], zeta, xi_labels[None, :, :], zeta_labels) # (n_samples, m, 1)
+
+    loss_outputs = loss(theta, zeta, zeta_labels)
+    exps_coefs = loss_outputs - lam * c
+    exps_coefs /= epsilon
+
+    minus_full_grads = non_overflow_exp_mean(exps_coefs, c)
+    grad_estimate = rho - minus_full_grads.mean()
+    lr = lr_decay_schedule(t)
+    return -lr * grad_estimate
+
+def step_theta_wol(xi, zeta, theta, lam, cost, loss_fns, step_id, epsilon):
+    loss, loss_grad = loss_fns
+    grads_theta_loss = loss_grad(theta, zeta) # array of shape (n_samples, m, d)
+
+    c = cost(xi[None, :, :], zeta) # (n_samples, m, 1)
+    loss_outputs = loss(theta, zeta)
+    exps_coefs = loss_outputs - lam * c
+    exps_coefs /= epsilon
+
+    full_grads = non_overflow_exp_mean(exps_coefs, grads_theta_loss)
+    grad_estimate = full_grads.mean(axis=0)
+
+    lr = lr_decay_schedule(step_id)
+    return -lr * grad_estimate
+
+def step_theta_wl(xi, xi_labels, zeta, zeta_labels, theta, lam, cost, loss_fns, step_id, epsilon):
+    loss, loss_grad = loss_fns
+    grads_theta_loss = loss_grad(theta, zeta, zeta_labels) # array of shape (n_samples, m, d)
+
+    c = cost(xi[None, :, :], zeta, xi_labels[None, :, :], zeta_labels) # (n_samples, m, 1)
+    loss_outputs = loss(theta, zeta, zeta_labels)
+    exps_coefs = loss_outputs - lam * c
+    exps_coefs /= epsilon
+
+    full_grads = non_overflow_exp_mean(exps_coefs, grads_theta_loss)
+    grad_estimate = full_grads.mean(axis=0)
+
+    lr = lr_decay_schedule(step_id)
+    return -lr * grad_estimate
+
+
+def step_wgx_wol(xi, zeta, theta, lam, cost, loss_fns, t, rho_eps):
+    rho, epsilon = rho_eps
+
+    K = 5
+    step_theta = step_theta_wol(xi, zeta, theta, lam, cost, loss_fns, t, epsilon)
+    for k in range(1, K):
+        step_theta = step_theta_wol(xi, zeta, theta, lam, cost, loss_fns, t+k, epsilon)
+        theta += step_theta
+    step_lambda = step_lam_wol(xi, zeta, theta, lam, cost, loss_fns[0], t, rho, epsilon)
+    lam += step_lambda
+    return theta, lam, (step_theta, step_lambda)
+
+def step_wgx_wl(xi, xi_labels, zeta, zeta_labels, theta, lam, cost, loss_fns, t, rho_eps):
+    rho, epsilon = rho_eps
+
+    K = 5
+    step_theta = step_theta_wl(xi, xi_labels, zeta, zeta_labels, theta, lam, cost, loss_fns, t, epsilon)
+    for k in range(1, K):
+        step_theta = step_theta_wl(xi, xi_labels, zeta, zeta_labels, theta, lam, cost, loss_fns, t+k, epsilon)
+        theta += step_theta
+    step_lambda = step_lam_wl(xi, xi_labels, zeta, zeta_labels, theta, lam, cost, loss_fns[0], t, rho, epsilon)
+    lam += step_lambda
+    return theta, lam, (step_theta, step_lambda)
+# ##############################################################
+
+
+def wgx_v2_wo_labels(d, data_structure, m, rho_eps, lam_0, n_samples, cost, loss_fns, fit_intercept, opt_cond):
+
+    xi, theta, zeta = prepare_data(data_structure.samples, m, d, n_samples, rho_eps[1], fit_intercept)
+
+    c = lambda samples_x, z_x, *_: cost(samples_x, z_x)
+
+    t = 1
+    lam = lam_0
+    while t > 0:
+        theta, lam, grads = step_wgx_wol(xi, zeta, theta, lam, c, loss_fns, t, rho_eps)
+        if opt_cond(grads, t): break
+        else: t += 1
+    return theta, lam
+
+
+def wgx_v2_w_labels(d, data_structure, m, rho_eps, lam_0, n_samples, cost, loss, fit_intercept, opt_cond):
+
+    xi, theta, zeta = prepare_data(data_structure.samplesX, m, d, n_samples, rho_eps[1], fit_intercept)
+    xi_labels = data_structure.samplesY[:, None]
+    zeta_labels = sample_pi_0(rho_eps[1], n_samples, xi_labels)
+
+    c = lambda samples_x, z_x, samples_y, z_y: cost(samples_x, z_x, samples_y, z_y)
+
+    t = 1
+    lam = lam_0
+    while t > 0:
+        theta, lam, grads = step_wgx_wl(xi, xi_labels, zeta, zeta_labels, theta, lam, c, loss, t, rho_eps)
+        if opt_cond(grads, t): break
+        else: t += 1
+    return theta, lam
+
+def WangGaoXie_v2(WDROProblem, epsilon=0.1, n_samples=50, fit_intercept=False, opt_cond=OptCond(2)):
+    """ Algorithm of Wang et al. but with epsilon >0 and delta = 0 (regularization in the objective)"""
+
+    n = WDROProblem.n
+    d = WDROProblem.d
+
+    no_labels = WDROProblem.dLabel == 0
+
+    data_structure = WDROProblem.P
+
+    m = WDROProblem.P.m
+    rho =  WDROProblem.rho
+
+    lamL = 1e-10
+    lamU = 1e10
+    lam = (lamL+lamU)/2.0
+
+    lam_eps = 1e2
+
+    # TODO: fix
+    lam = 1e1
+
+    loss = WDROProblem.loss
+    if no_labels:
+        theta, lam = wgx_v2_wo_labels(
+                d,
+                data_structure,
+                m,
+                (rho, epsilon),
+                lam,
+                n_samples,
+                WDROProblem.c,
+                (loss.value, loss.grad_theta),
+                fit_intercept,
+                opt_cond
+            )
+    else:
+        theta, lam = wgx_v2_w_labels(
+                d,
+                data_structure,
+                m,
+                (rho, epsilon),
+                lam,
+                n_samples,
+                WDROProblem.c,
+                (loss.valueSplit, loss.grad_thetaSplit),
+                fit_intercept,
+                opt_cond
+            )
+    if fit_intercept:
+        return theta[1:], theta[0], lam
+    else:
+        return theta, None, lam
 
 def WangGaoXie_v1(WDROProblem, epsilon=0.1, Nsamples = 50,fit_intercept=False):
     """ Algorithm of Wang et al. but with epsilon >0 and delta = 0 (regularization in the objective)"""
--- skwdro/solvers/utils.py
+++ skwdro/solvers/utils.py
@@ -15,3 +15,32 @@ def weightedExpAverage(a,b):
     
     return num/den
     
+
+def non_overflow_exp_mean(coefs, multipliers):
+    r"""
+    Given coefficients :math:`b_i` that we wish to exponentiate and weight coefficients :math:`a_i` to put before, we wish to compute the weighted mean of the coefs' exponentials:
+
+    .. math::
+        \frac{1}{N}\sum_{i<N}a_ie^{b_i}=e^{b_{max}-\ln(N)}\sum_{i<N}a_ie^{b_i-b_{max}}
+
+    The average is taken on the first dimension of the arrays (n_samples in the shape, i.e. the number of zeta samples).
+
+    Parameters
+    ==========
+    coefs: shape (n_samples, m, 1)
+        coefficients :math:`b_i` to exponentiate
+    multipliers: (n_samples, m, d)
+        weights of the average denoted :math:`a_i`
+
+    Returns
+    =======
+    avg: (m, d)
+        properly scaled averaged exponentials
+    """
+    coef_max = coefs.max(axis=0, keepdims=True) # (1, m, 1)
+    exps = np.exp(
+            coefs - coef_max
+        )
+    scaling = np.sum(exps)
+    unscaled_avg = np.einsum("ijk,ijk->jk", multipliers, coefs)
+    return unscaled_avg / scaling
