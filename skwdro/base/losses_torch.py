8cd70ae086b9c12cccfe5f21369a14bd3834c5a7
Vincent Florian <florianvincent@free.fr>
2023-05-25 16:44:39 +0200

    [FEAT] Logistic torch loss
    BCE-based torch loss for logistic regression.
    Warning:No tests
    
    Note: {-1, 1} have been chosen as labels for constistency

--- skwdro/base/losses_torch.py
+++ skwdro/base/losses_torch.py
@@ -1,4 +1,5 @@
-import torch
+import torch as pt
+import torch.nn as nn
 
 class Loss:
     """ Base class for loss functions """
@@ -15,7 +16,7 @@ class NewsVendorLoss_torch(Loss):
         self.name = name
 
     def value(self,theta,xi):
-        return self.k*theta-self.u*torch.minimum(theta,xi)
+        return self.k*theta-self.u*pt.minimum(theta,xi)
 
 class WeberLoss_torch(Loss):
 
@@ -23,4 +24,23 @@ class WeberLoss_torch(Loss):
         self.name = name
 
     def value(self,y,x,w):
-        return w*torch.norm(x-y)
+        return w*pt.linalg.norm(x-y)
+
+class LogisticLoss(Loss, nn.Module):
+    def __init__(self, d: int=0, fit_intercept: bool=False) -> None:
+        super(LogisticLoss, self).__init__()
+        assert d > 0, "Please provide a valid data dimension d>0"
+        self.linear = nn.Linear(d, 1, bias=fit_intercept)
+        self.classif = nn.Tanh()
+        self.L = nn.BCEWithLogitsLoss()
+
+    def forward(self, X):
+        coefs = self.linear(X)
+        return self.classif(coefs), coefs
+
+    def value(self, X, y):
+        _, coefs = self.__call__(X)
+        return self.L(
+                coefs,
+                (y == 1).long(),
+                reduction='none')
