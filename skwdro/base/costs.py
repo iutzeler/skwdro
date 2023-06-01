34977288c47b31f691d828b6c88df762e0e98696
Vincent Florian <florianvincent@free.fr>
2023-06-01 15:24:31 +0200

    [FEAT][WIP] Adding cost-based samplers

--- skwdro/base/costs.py
+++ skwdro/base/costs.py
@@ -27,6 +27,15 @@ class Cost:
     def __str__(self) -> str:
         return "Cost named " + self.name + " using as data: " + ENGINES_NAMES[self.engine]
 
+    def sampler(self, xi, xi_labels, epsilon):
+        return self._sampler_data(xi, epsilon), self._sampler_labels(xi_labels, epsilon)
+
+    def _sampler_data(self, xi, epsilon):
+        raise NotImplementedError()
+
+    def _sampler_labels(self, xi_labels, epsilon):
+        raise NotImplementedError()
+
 
 class NormCost(Cost):
     """ p-norm to some power """
