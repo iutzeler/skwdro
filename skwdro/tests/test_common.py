d20c10efa5378156a8dc79a52b98630eb4126a63
Waiss Azizian <waiss.azizian@univ-grenoble-alpes.fr>
2023-06-02 14:00:39 +0200

    Switch to hatch for packaging

--- skwdro/tests/test_common.py
+++ skwdro/tests/test_common.py
@@ -2,14 +2,10 @@ import pytest
 
 from sklearn.utils.estimator_checks import check_estimator
 
-from skwdro import TemplateEstimator
-from skwdro import TemplateClassifier
-from skwdro import TemplateTransformer
-
 
 @pytest.mark.parametrize(
     "estimator",
-    [TemplateEstimator(), TemplateTransformer(), TemplateClassifier()]
+    []
 )
 def test_all_estimators(estimator):
     return check_estimator(estimator)
