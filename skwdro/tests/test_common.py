f07d4db0d3d021d36c53e87cad21dd44f1d09c67
Franck Iutzeler <franck.iutzeler@gmail.com>
2023-06-05 12:00:26 +0200

    All tests ok (#16)

--- skwdro/tests/test_common.py
+++ skwdro/tests/test_common.py
@@ -2,13 +2,14 @@ import pytest
 
 from sklearn.utils.estimator_checks import check_estimator
 
-from skwdro.operations_research import Weber
+from skwdro.operations_research import Weber, Portfolio
 
 Weber_rob_est = Weber()
+Rob_Portfolio = Portfolio()
 
 @pytest.mark.parametrize(
     "estimator",
-    [Weber_rob_est]
+    [Weber_rob_est, Rob_portfolio]
 )
 def test_all_estimators(estimator):
     return check_estimator(estimator)
