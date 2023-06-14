bd8e846ca55e1062dfc1d4b59302e0e535d71372
Vincent Florian <florianvincent@free.fr>
2023-06-14 17:35:09 +0200

    [FIX][REFACT] Minor renamings

--- skwdro/linear_models/_linear_regression.py
+++ skwdro/linear_models/_linear_regression.py
@@ -172,7 +172,6 @@ class LinearRegression(BaseEstimator, RegressorMixin):
             self.coef_, self.intercept_, self.dual_var_ = entTorch.solve_dual(
                     self.problem_,
                     sigma=self.solver_reg,
-                    fit_intercept=self.fit_intercept,
                 )
         elif self.solver == "entropic_torch_pre":
             self.problem_.loss = DualPreSampledLoss(
@@ -186,7 +185,6 @@ class LinearRegression(BaseEstimator, RegressorMixin):
             self.coef_, self.intercept_, self.dual_var_ = entTorch.solve_dual(
                     self.problem_,
                     sigma=self.solver_reg,
-                    fit_intercept=self.fit_intercept,
                 )
         elif self.solver=="dedicated":
             self.coef_ , self.intercept_, self.dual_var_ = spS.WDROLinRegSpecificSolver(
