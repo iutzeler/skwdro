2e017d40030e9cc8e906ba8772276358e9d1658c
Franck Iutzeler <franck.iutzeler@gmail.com>
2023-06-15 16:14:55 +0200

    Application/portfolio regstructure (#40)
    * Essential modifications on files linked to the branch
    
    * Added HybridSGD untracked files
    
    * Added modified files
    
    * fixing hybrid_sgd
    
    * Revert "fixing hybrid_sgd"
    
    This reverts commit 35124748d41409479ce129135cdf212ad1de2315.
    
    * Revert "Added modified files"
    
    This reverts commit 71572379d996466e13affbeb9e393734c45ccd24.
    
    * fixing hybrid sgd
    
    ---------
    
    Co-authored-by: ALAN LEBREUX <lebreuxalan@gmail.com>

---
+++ skwdro/base/samplers/torch/portfolio_sampler.py
@@ -0,0 +1,41 @@
+from typing import Optional, Union
+import torch as pt
+import torch.distributions as dst
+
+from skwdro.base.samplers.torch.base_samplers import IsOptionalCovarianceSampler, NoLabelsSampler
+
+class PortfolioNormalSampler(NoLabelsSampler, IsOptionalCovarianceSampler):
+    data_s: dst.MultivariateNormal
+    def __init__(self, xi, *,
+                 sigma: Optional[Union[pt.Tensor, float]]=None,
+                 tril: Optional[pt.Tensor]=None,
+                 prec: Optional[pt.Tensor]=None,
+                 cov: Optional[pt.Tensor]=None
+                 ):
+        super(PortfolioNormalSampler, self).__init__(
+                dst.MultivariateNormal(
+                    loc=xi,
+                    **self.init_covar(xi.size(-1), sigma, tril, prec, cov)
+                )
+            )
+
+    def reset_mean(self, xi, xi_labels):
+        self.__init__(xi, tril=self.data_s._unbroadcasted_scale_tril)
+
+class PortfolioLaplaceSampler(NoLabelsSampler, IsOptionalCovarianceSampler):
+    data_s: dst.MultivariateNormal #To adapt (there is a priori no MultivariateLaplace in Pytorch)
+    def __init__(self, xi, *,
+                 sigma: Optional[Union[pt.Tensor, float]]=None,
+                 tril: Optional[pt.Tensor]=None,
+                 prec: Optional[pt.Tensor]=None,
+                 cov: Optional[pt.Tensor]=None
+                 ):
+        super(PortfolioNormalSampler, self).__init__(
+                dst.MultivariateNormal( #Idem
+                    loc=xi,
+                    **self.init_covar(xi.size(-1), sigma, tril, prec, cov)
+                )
+            )
+
+    def reset_mean(self, xi, xi_labels):
+        self.__init__(xi, tril=self.data_s._unbroadcasted_scale_tril)
---
+++ tests/pytorch_tests/test_hybrid_sgd.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import pytest
+
+import skwdro.solvers.hybrid_sgd as hybrid_sgd
+
+class DummyModule(nn.Module):
+	def __init__(self, a, b):
+		super(DummyModule, self).__init__()
+		self.a = nn.Parameter(a)
+		self.b = nn.Parameter(b)
+	
+	def forward(self):
+		return torch.linalg.norm(self.a)**2 + torch.linalg.norm(self.b)**2
+
+@pytest.fixture
+def model():
+	return DummyModule(torch.rand(1), torch.rand(1))
+
+@pytest.fixture
+def lr():
+	return 1.5*torch.rand(1).item()
+
+lr_a = lr
+lr_b = lr
+
+@pytest.fixture
+def optimizer(model, lr_a, lr_b):
+	return hybrid_sgd.HybridSGD([
+		{'params': [model.a], 'lr':lr_a, 'mwu':True},
+		{'params': [model.b], 'lr':lr_b, 'non_neg':True}
+		], lr=lr_a)
+
+def test_hybrid_sgd(model, optimizer, lr_a, lr_b):
+	optimizer.zero_grad()
+
+	loss = model.forward()
+	loss.backward()
+
+	with torch.no_grad():
+		exp_a = model.a * torch.exp(-lr_a*model.a.grad)
+		exp_b = torch.clip(model.b - lr_b*model.b.grad, 0, None)
+	
+	optimizer.step()
+
+	assert exp_a == pytest.approx(model.a.item())
+	assert exp_b == pytest.approx(model.b.item())
+	
+	optimizer.zero_grad(set_to_none=False)
+
+	assert model.a.grad.item() == 0.
+	assert model.b.grad.item() == 0.
+
+
+
+
+
