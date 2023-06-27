da8280c1e72e85e8bd22f26a829ccd796ab14601
ALAN LEBREUX <lebreuxalan@gmail.com>
2023-06-27 14:35:25 +0200

    Added rho tuner + changer structure in portfolio files

--- examples/plot_portfolio_display.py
+++ examples/plot_portfolio_display.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import time
 import matplotlib.pyplot as plt
 
-def plot_histograms(N=30, nb_simulations=10000, rho=1e-2, estimator_solver="dedicated", compute=True):
+def plot_histograms(N=30, nb_simulations=10000, rho=1e-2, estimator_solver="dedicated", compute=True, rho_tuning=True):
     '''
     Plots Kuhn's histograms that were presented at the DTU CEE Summer School 2018.
     Setting rho=0 stands for the SAA method of resolution of the stochastic problem.
@@ -12,20 +12,45 @@ def plot_histograms(N=30, nb_simulations=10000, rho=1e-2, estimator_solver="dedi
 
     start = time.time()
 
-    filename = parallel_compute_histograms(N=N, nb_simulations=nb_simulations, estimator_solver=estimator_solver, compute=compute)
+    filename, rho_filename = parallel_compute_histograms(N=N, nb_simulations=nb_simulations, 
+                                           estimator_solver=estimator_solver, compute=compute, rho_tuning=rho_tuning)
 
     with open (filename, 'rb') as f:
         eval_data_train = np.load(f)
         eval_data_test = np.load(f)
         eval_data_adv_test = np.load(f)
     f.close()
+
+    with open (rho_filename, 'rb') as f:
+        tuned_rho_data = np.load(f)
+    f.close()
+
+    rho_possible_values = [10**(-i) for i in range(4,-4,-1)]
+
+    #Create an histogram that shows the values taken for the tuning of rho
+    plt.figure()
+    plt.xticks(rho_possible_values)
+    plt.xscale("log")
+    plt.xlabel("Rho values")
+    plt.ylabel("Number of occurences")
+    plt.title("Distribution of rho values taken when tuning rho")
+    sns.histplot(data=tuned_rho_data, bins=len(rho_possible_values), stat="count", discrete=True)
+
+    #Saving the histogram
+    home_dir = os.path.expanduser("~")
+    image_path = os.path.join(home_dir, "rho_values.png")
+    plt.savefig(image_path)
     
-    #Create the histograms
+    #Create an histogram showing the performance of the model on train, test and adversarial data
+    plt.figure()
     plt.xlabel("Mean-risk objective")
-    if rho == 0:
-        plt.title("SAA Solution with scarce data (Kuhn 2017)")
+    if rho_tuning is False: 
+        if rho == 0:
+            plt.title("SAA Solution with scarce data (Kuhn 2017)")
+        else:
+            plt.title("WDRO Solution with scarce data (Kuhn 2017) with rho = %f" %rho)
     else:
-        plt.title("DRO Solution with scarce data (Kuhn 2017) with rho = %f" %rho)
+        plt.title("WDRO Solution with scarce data (Kuhn 2017) with tuning of rho")
     end = time.time()
     sns.histplot(data=eval_data_train, bins=20, stat="probability", color="green", multiple="dodge", label="In-sample")
     sns.histplot(data=eval_data_test, bins=20, stat="probability", color="red", multiple="dodge", label="Out-of-sample")
