5e482d311347e0bb87898aa1c7ecc782bebc16a5
ALAN LEBREUX <lebreuxalan@gmail.com>
2023-06-28 13:14:22 +0200

    Added basic tests for prtfolio constraints on the xi_i

--- examples/portfolio.py
+++ examples/portfolio.py
@@ -27,7 +27,12 @@ print("Value of the samples:", X)
 
 #Creating the estimator and solving the problem
 estimator = Portfolio(solver="dedicated", rho=1e-10)
-estimator.fit(X)
+estimator.fit(X, C=C, d=d)
+
+#rho_tuner= BlanchetRhoTunedEstimator(estimator)
+#rho_tuner.fit(X=X, y=None)
+
+print("Best value for rho: ", estimator.rho_)
 
 #rho_tuner= BlanchetRhoTunedEstimator(estimator)
 #rho_tuner.fit(X=X, y=None)
