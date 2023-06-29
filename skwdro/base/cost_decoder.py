9aa15c2ed09dd50203d2b2636a14f3b2d9c64e1b
Vincent Florian <florianvincent@free.fr>
2023-06-29 15:50:07 +0200

    [REFACT][FEAT] Added codes for costs to simplify
    See the estimator's argument documentation to understand the "code"'s
    grammar.

---
+++ skwdro/base/cost_decoder.py
@@ -0,0 +1,18 @@
+from .costs import Cost, NormCost as npNormCost, NormLabelCost as npNormLabelCost
+from .costs_torch import NormCost as ptNormCost, NormLabelCost as ptNormLabelCost
+
+def cost_from_str(code: str) -> Cost:
+    engine_, id_, power_, type_ = code.split('-')
+    if engine_ == 't':
+        if id_ == "NC":
+            return ptNormCost(p=float(type_), power=float(power_), name=code)
+        elif id_ == "NLC":
+            return ptNormLabelCost(p=float(type_), power=float(power_), kappa=1e8, name=code)
+        else: raise ValueError("Cost code invalid: "+code)
+    elif engine_ == 'n':
+        if id_ == "NC":
+            return npNormCost(p=float(type_), power=float(power_), name=code)
+        elif id_ == "NLC":
+            return npNormLabelCost(p=float(type_), power=float(power_), kappa=1e8, name=code)
+        else: raise ValueError("Cost code invalid: "+code)
+    else: raise ValueError("Cost code invalid: "+code)
