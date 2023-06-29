76b88591992583ace934c1e07ef13c0a995bb350
Vincent Florian <florianvincent@free.fr>
2023-06-29 10:19:38 +0200

    [REFACT] Refactoring on the "costs" side
    Stealth changes to the cost implem, see next commit

---
+++ skwdro/base/costs_torch/__init__.py
@@ -0,0 +1,6 @@
+from . import normcost, normlabelcost
+
+from .normcost import NormCost, Cost
+from .normlabelcost import NormLabelCost
+
+__all__ = ["NormCost", "NormLabelCost", "Cost"]
---
+++ skwdro/base/costs_torch/normcost.py
@@ -0,0 +1,54 @@
+from typing import Optional
+from types import NoneType
+
+import torch as pt
+import torch.distributions as dst
+
+from skwdro.base.costs import Cost
+
+class NormCost(Cost):
+    """ p-norm to some power, with torch arguments
+    """
+    def __init__(self, p: float=1., power: float=1., name: Optional[str]=None):
+        r"""
+        Norm to represent the ground cost of type :math:`p`.
+        It represents a distance depending on :math:`p`:
+            * for :math:`p=1`: Manhattan
+            * for :math:`p=2`: Euclidean distance
+            * for :math:`p=\infty`: Sup-norm
+        """
+        super().__init__(name="Norm" if name is None else name, engine="pt")
+        self.p = p
+        self.power = power
+
+    def value(self, xi: pt.Tensor, zeta: pt.Tensor, xi_labels: NoneType=None, zeta_labels: NoneType=None):
+        r"""
+        Cost to displace :math:`\xi` to :math:`\zeta` in :math:`mathbb{R}^n`.
+
+        Parameters
+        ----------
+        xi : Tensor
+            Data point to be displaced
+        zeta : Tensor
+            Data point towards which ``xi`` is displaced
+        """
+        diff = xi - zeta
+        return pt.norm(diff, p=self.p, dim=-1, keepdim=True)**self.power
+
+    def _sampler_data(self, xi, epsilon):
+        if self.power == 1:
+            if self.p == 1:
+                return dst.Laplace(
+                            loc=xi,
+                            scale=epsilon
+                        )
+            elif self.p == 2:
+                return dst.MultivariateNormal(
+                        loc=xi,
+                        scale_tril=epsilon*pt.eye(xi.size(-1))
+                    )
+            else: raise NotImplementedError()
+        else: raise NotImplementedError()
+
+    def _sampler_labels(self, xi_labels, epsilon):
+        return None
