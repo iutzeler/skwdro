661cf34ad31e0a6fe83c69d97f2fce198f6e2f89
Victor Mercklé <11957227+vmerckle@users.noreply.github.com>
2023-07-13 10:10:17 +0200

    Added comp plot with sinus heavy data (#59)

--- examples/plot_shallowNet_1D_GDvsRobust.py
+++ examples/plot_shallowNet_1D_GDvsRobust.py
@@ -1,9 +1,10 @@
 """
 ===================
-Neural Network
+Plotting Neural Network with one dimensional data
 ===================
 
-An example of Neural Network.
+An example plot to show a case where robustness gives a much better test loss
+in a regression setting.
 """
 
 # stdlib
@@ -28,6 +29,7 @@ from sklearn.linear_model import LinearRegression
 # skwdro
 from skwdro.neural_network import ShallowNet as RobustShallowNet
 
+# Standard shallow neural network
 class AuxNet(nn.Module):
     def __init__(self, d, n_neurons, fit_intercept, ly1=None, ly2=None):
         super(AuxNet, self).__init__()
@@ -97,7 +99,6 @@ class ShallowNet:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", default=None, type=int, help="if set, will use a fixed seed instead of a random one each run")
-    parser.add_argument("--plot", action="store_true", help="plot one dimensional data")
     args = parser.parse_args()
 
     # seed stuff
@@ -112,12 +113,9 @@ if __name__ == '__main__':
     pt.use_deterministic_algorithms(True)
 
     dim_data, n_data, n_neurons = 1, 10, 100
-    scaling = 1e-2
-    x0 = rng.standard_normal(dim_data)
+    x0 = rng.standard_normal(dim_data) # don't @ me
 
     s = 1.3
-    f = lambda x : x.dot(x0)
-    f = lambda x : np.sin(x*x0*2).flatten()
     f = lambda x : (np.sin(x/s)+0.1*np.sin(100*x/s)).flatten()
 
     X_train = rng.standard_normal((n_data, dim_data))
@@ -128,20 +126,19 @@ if __name__ == '__main__':
     X_test = np.array([np.pi*2/100 * i for i in range(-n_data//2, n_data//2)])[:, None]*s
     y_test = f(X_test)
 
-    #X_train, X_test, y_train, y_test = train_test_split(X,y)
-
     def Loss(yhat, y):
         if yhat.shape != y.shape:
             # print(f"for info, yhat.shape != y.shape: {yhat.shape} != {y.shape}")
             pass
         return np.square(yhat.flatten()-y.flatten()).sum()/len(yhat)
 
-
+    # Initialiazing the two layers of the network, used for both optimizers
+    scaling = 1e-2
     ly1 = rng.uniform(-scaling, scaling, size=(n_neurons, dim_data+1))
     ly2 = rng.uniform(-scaling, scaling, size=(1, n_neurons))
 
     # Standard pytorch shallow network
-    print("Fitting Standard pytorch shallow network")
+    print("Fitting the Standard pytorch shallow network")
     mdl = ShallowNet(dim_data, n_neurons, fit_intercept=True, ly1=ly1, ly2=ly2, lr=1e-2)
     mdl.fit(X_train, y_train, iterlimit=20000, timelimit=None, gradcheck=False) # deter
     #mdl.fit(X_train, y_train, iterlimit=1000, timelimit=20, gradcheck=True) # will differ
@@ -149,50 +146,15 @@ if __name__ == '__main__':
     print(f"relu Shallownet Pytorch: TRAIN {gd_fit_train:.6f} - TEST {gd_fit_test:.6f}")
     ly1_gd, ly2_gd = mdl.params() # (n_neurons, dim_data+1), (1, n_neurons)
 
-    if False: #TODO remove
-        lact_gd = np.sort(np.array([-w2/w1 for w1, w2 in ly1_gd]))
-        lact_gd = np.array([[x, 1] for x in lact_gd])
-        points_gd = (np.maximum(lact_gd@ly1_gd.T, 0) @ ly2_gd.T).flatten()
-        fig = plt.figure(figsize=(10,4))
-        ax = fig.add_subplot()
-        ax.axhline(y=0, color="black", linestyle="-", alpha=0.7, linewidth=0.4)
-        ax.axvline(x=0, color="black", linestyle="-", alpha=0.7, linewidth=0.4)
-        ax.grid(True, alpha=0.2)
-        ax.scatter(X_train.flatten(), y_train.flatten(), marker="o", color="red", s=80, alpha=1, label="train data")
-        ax.plot(lact_gd[:, 0], points_gd, linestyle="-", marker="x", label="gd output")
-        X_test_sort = np.sort(X_test.flatten())
-        y_test_sort = f(X_test_sort)
-        ax.plot(X_test_sort,y_test_sort, linestyle="-", marker="|", label="test data")
-        ax.set_xlim(X_train[0].flatten(), X_train[-1].flatten())
-        ax.set_ylim(-1, 1)
-        plt.legend()
-        plt.show()
-        sys.exit(0)
-
     # Skwdro robust shallow network
-    print("Skwdro robust shallow network")
+    print("Fitting the Skwdro robust shallow network")
     mdl = RobustShallowNet(rho=0.1,solver="entropic_torch",fit_intercept=True, n_neurons=n_neurons, ly1=ly1, ly2=ly2)
     mdl.fit(X_train, y_train)
     ro_fit_train, ro_fit_test = Loss(mdl.predict(X_train), y_train), Loss(mdl.predict(X_test), y_test)
     print(f"Skwdro TRAIN {ro_fit_train:.6f} - TEST {ro_fit_test:.6f}")
     ly1_ro, ly2_ro = mdl.params() # (n_neurons, dim_data+1), (1, n_neurons)
 
-    # Sklearn linear regression
-    mdl = LinearRegression()
-    mdl.fit(X_train, y_train)
-    fit_train, fit_test = Loss(mdl.predict(X_train), y_train), Loss(mdl.predict(X_test), y_test)
-    print(f"sklearn linear: TRAIN {fit_train:.6f} - TEST {fit_test:.6f}")
-
-    # Sanity check
-    X_train_b = np.array([[x, 1] for x in X_train.flatten()])
-    points = np.maximum(X_train_b@ly1_gd.T, 0) @ ly2_gd.T
-    gd_fit_train_2 = np.linalg.norm(points.flatten()-y_train)**2/len(y_train)
-    if abs(gd_fit_train - gd_fit_train_2)/gd_fit_train_2 > 1e-5: # assert model to ly1/ly2 is correct
-        print("(true gd_fit_train_2)", gd_fit_train, "!=", gd_fit_train_2)
-
-    ## Plotting 1-dimensional data
-    if dim_data!= 1 and not args.plot:
-        sys.exit(0)
+    print("Preparing plot..")
     # Compute points to plot the output for GD and "robust GD"
     lact_gd = np.sort(np.array([-w2/w1 for w1, w2 in ly1_gd]))
     lact_gd = np.array([[x, 1] for x in lact_gd])
@@ -207,16 +169,15 @@ if __name__ == '__main__':
     ax.axhline(y=0, color="black", linestyle="-", alpha=0.7, linewidth=0.4)
     ax.axvline(x=0, color="black", linestyle="-", alpha=0.7, linewidth=0.4)
     ax.grid(True, alpha=0.2)
+    # Data points
     ax.scatter(X_train.flatten(), y_train.flatten(), marker="o", color="red", s=80, alpha=1, label="train data")
-    ax.plot(lact_gd[:, 0], points_gd, linestyle="-", marker="x", label="gd output")
-    ax.plot(lact_ro[:, 0], points_ro, linestyle="-", marker="x", label="ro output")
-    ax.text(0.05, 0.9, f"Test loss\ngd:{gd_fit_test:.5f}\nro: {ro_fit_test:.5f}", transform=ax.transAxes)
-    X_test_sort = np.sort(X_test.flatten())
+    # We only need to plot points where the output change its slope.
+    ax.plot(lact_gd[:, 0], points_gd, linestyle="-", marker="x", label="Adam optimizer")
+    ax.plot(lact_ro[:, 0], points_ro, linestyle="-", marker="x", label="Wdro optimizer")
+    ax.text(0.05, 0.9, f"Test loss\n{gd_fit_test:.5f} - Adam\n{ro_fit_test:.5f} - Wdro", transform=ax.transAxes)
+    X_test_sort = np.sort(X_test.flatten()) # of course
     y_test_sort = f(X_test_sort)
     ax.plot(X_test_sort,y_test_sort, linestyle="-", marker="|", label="test data")
-    #ax.set_xlim(-10, 10)
-    #ax.set_ylim(-10, 10)
-
     ax.set_xlim(X_train[0].flatten(), X_train[-1].flatten())
     ax.set_ylim(-1, 1)
     plt.legend()
