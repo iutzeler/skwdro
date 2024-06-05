574e6a6d2ce32c70dcfb7125ae424a5c96fedda6
Florian VINCENT <florian.vincent@inria.fr>
2024-06-05 15:52:12 +0200

    [FIX] Fixed shape issues with the example on moon

--- examples/custom_models/utils/plotting.py
+++ examples/custom_models/utils/plotting.py
@@ -7,20 +7,20 @@ import matplotlib.pyplot as plt
 
 # TK - this could go in the helper_functions.py and be explained there
 def plot_decision_boundary(model, X, y):
-  
+
     # Put everything to CPU (works better with NumPy + Matplotlib)
     model.to("cpu")
     X, y = X.to("cpu"), y.to("cpu")
 
-    # Source - https://madewithml.com/courses/foundations/neural-networks/ 
+    # Source - https://madewithml.com/courses/foundations/neural-networks/
     # (with modifications)
     x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
     y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
-    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 101), 
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 101),
                          np.linspace(y_min, y_max, 101))
 
     # Make features
-    X_to_pred_on = torch.from_numpy(np.column_stack((xx.ravel(), yy.ravel()))).float()
+    X_to_pred_on = torch.from_numpy(np.column_stack((xx.ravel(), yy.ravel()))).to(X)
 
     # Make predictions
     model.eval()
@@ -30,9 +30,9 @@ def plot_decision_boundary(model, X, y):
     # Test for multi-class or binary and adjust logits to prediction labels
     if len(torch.unique(y)) > 2:
         y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1) # mutli-class
-    else: 
+    else:
         y_pred = torch.round(torch.sigmoid(y_logits)) # binary
-    
+
     # Reshape preds and plot
     y_pred = y_pred.reshape(xx.shape).detach().numpy()
     plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
