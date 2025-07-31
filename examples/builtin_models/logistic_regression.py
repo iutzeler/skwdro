6492bf9326c3c94d6ed148ecdbb146b3b32f4f31
Florian VINCENT <florian.vincent@inria.fr>
2025-07-31 23:21:45 +0200

    [DOC] Added some examples and eddited some of them

--- examples/builtin_models/logistic_regression.py
+++ examples/builtin_models/logistic_regression.py
@@ -19,17 +19,17 @@ from utils.classifier_comparison import plot_classifier_comparison
 # Setup
 # ~~~~~
 
-n = 200 # Total number of samples
+n = 100 # Total number of samples
 n_train = n // 2 # Number of training samples
 n_test = n - n_train # Number of test samples
 
 datasets = []
 
-# Moons dataset
-dataset = make_moons(n_samples=n, noise=0.1)
-Xtrain, Xtest, ytrain, ytest = train_test_split(*dataset, train_size=n_train, test_size=n_test)
-train_dataset, test_dataset = (Xtrain, ytrain), (Xtest, ytest)
-datasets.append((train_dataset, test_dataset))
+# # Moons dataset
+# dataset = make_moons(n_samples=n, noise=0.1)
+# Xtrain, Xtest, ytrain, ytest = train_test_split(*dataset, train_size=n_train, test_size=n_test)
+# train_dataset, test_dataset = (Xtrain, ytrain), (Xtest, ytest)
+# datasets.append((train_dataset, test_dataset))
 
 # Fix centers for blobs dataset
 _, _, centers = make_blobs(centers=2, return_centers=True) # type: ignore
@@ -40,20 +40,18 @@ Xtrain, Xtest, ytrain, ytest = train_test_split(*dataset, train_size=n_train, te
 train_dataset, test_dataset = (Xtrain, ytrain), (Xtest, ytest)
 datasets.append((train_dataset, test_dataset))
 
-# Blobs dataset with std=4
-dataset = make_blobs(n_samples=n, centers=centers, cluster_std=4)
-Xtrain, Xtest, ytrain, ytest = train_test_split(*dataset, train_size=n_train, test_size=n_test)
-train_dataset, test_dataset = (Xtrain, ytrain), (Xtest, ytest)
-datasets.append((train_dataset, test_dataset))
+# # Blobs dataset with std=4
+# dataset = make_blobs(n_samples=n, centers=centers, cluster_std=4)
+# Xtrain, Xtest, ytrain, ytest = train_test_split(*dataset, train_size=n_train, test_size=n_test)
+# train_dataset, test_dataset = (Xtrain, ytrain), (Xtest, ytest)
+# datasets.append((train_dataset, test_dataset))
 
 # %%
 # WDRO classifiers
 # ~~~~~~~~~~~~~~~~
 
-rhos = [10**i for i in range(-3, 0)]
-classifiers = []
-for rho in rhos:
-    classifiers.append(LogisticRegression(rho=rho))
+rhos = [10**i for i in (-5, 0)]#(-4, -2, 0)]
+classifiers = [LogisticRegression(rho=rho, solver_reg=1e-3) for rho in rhos]
 
 # %%
 # Make plot
---
+++ examples/studying_skwdro/README.rst
@@ -0,0 +1,4 @@
+Diving into the library and its settings
+========================================
+
+Those tutorials display briefly how the parameters of the wrapper function :py:class:`skwdro.torch.robustify` and all scikit-learn interfaces behave with respect to the main hyperparameters involved.
